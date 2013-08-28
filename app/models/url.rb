ce2314be78621a173d81045a984e82f21eebf4ee
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 09:21:26 -0500

    fixed validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,16 +1,20 @@
 class Url < ActiveRecord::Base
-  validates :long_url, inclusion: { in: %w(http https),
+  validates :long_url, format: { with: /(http:\/\/|https:\/\/)/,
     message: "Hey Bro thats not valid! fix that shit!" }
   validates :long_url, length: { minimum: 15,
     message: "Hey bro, Man the fuck up and type it out!!" }
 
   before_save :increment_counter, :generate_short_url
 
-  def increment
-    self.click_count += 1
+  def increment_counter
+    if self.click_count == nil
+      self.click_count = 0 
+    else
+      self.click_count += 1 
+    end
   end
 
-  def generate
+  def generate_short_url
     self.short_url = "#{rand(100..100000000)}" + ".com" 
   end
 end
--- db/migrate/20130828083129_urls.rb
+++ db/migrate/20130828083129_urls.rb
@@ -1,10 +1,10 @@
 class Urls < ActiveRecord::Migration
   def change
-    create_table :urls do |url|
+    create_table :urls do |t|
       t.string :long_url
       t.string :short_url
       t.integer :click_count
-      
+
       t.timestamps
     end
   end
